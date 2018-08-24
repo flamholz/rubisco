dfd3077e2becccee89376d5ef8a60f6459aae41d
Avi Flamholz <flamholz@gmail.com>
2018-08-24 15:03:19 -0700

    filter duplicate rubiscos.

--- notebooks/rubisco_data.py
+++ notebooks/rubisco_data.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 
 
-def load_rubisco_data(fname):
-	"""Loads the rubisco data.
+def load_rubisco_data():
+	"""Convencience the rubisco data.
 
 	Args:
 		fname: filename of csv to load data from.
@@ -14,6 +14,7 @@ def load_rubisco_data(fname):
 		raw_df, kin_df where raw is all the data and kin_df is the filtered data.
 	"""
 	# Load the data from Excel, remove data points that are not comparable.
+	fname = '../data/082418_rubisco_kinetics_merged.csv'
 	raw_kin_df = pd.read_csv(fname, index_col=0)
 
 	# Filtered DataFrame used for most plots below.
@@ -26,3 +27,19 @@ def load_rubisco_data(fname):
 	kin_df = kin_df[has_all]
 
 	return raw_kin_df, kin_df
+
+
+def filter_data(raw_kin_df):
+	"""There are intentionally some duplicates in the raw data 
+	because it includes Savir data directly copied from some primary references.
+
+	This filters it all out. 
+	"""
+	# Want to split data in various ways
+	savir_pmid = '20142476'
+	savir_df = raw_kin_df[raw_kin_df.pmid_or_doi == savir_pmid]
+	nonsavir_df = raw_kin_df[raw_kin_df.pmid_or_doi != savir_pmid]
+
+	dup_cols = ['KC', 'KC_SD', 'vC', 'vC_SD', 'S', 'S_SD', 'KO', 'KO_SD']
+	deduped = raw_kin_df.drop_duplicates(subset=dup_cols)
+	return deduped, savir_df, nonsavir_df
